77e6031e44c34271e6c0252921551c6f9d9a7c28
DiegoManucci <diegobizzotto2003@gmail.com>
2023-04-23 00:40:12 -0300

    Page Cadastro

--- app/app/src/main/java/com/unaerp/desafio/MainActivity.kt
+++ app/app/src/main/java/com/unaerp/desafio/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.unaerp.desafio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var cadastroMessage = findViewById<TextView>(R.id.cadastro_message)
+
+        cadastroMessage.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
